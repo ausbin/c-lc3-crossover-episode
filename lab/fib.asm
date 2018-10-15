a79d2edd94fe0c4489bf4ca025b86885f6fae5bd
Austin Adams <git@austinjadams.com>
2018-10-15 12:50:19 -0400

    Add C version

--- lab/fib.asm
+++ lab/fib.asm
@@ -12,6 +12,7 @@ and r1, r0, -1
 brz return
 add r0, r0, -1
 brz return
+
 add r6, r6, -1
 str r0, r6, 0
 jsr fib
@@ -19,6 +20,7 @@ ldr r1, r6, 0
 add r6, r6, 2
 ldr r0, r5, 4
 add r1, r0, r1
+
 return
 str r1, r5, 3
 ; teardown
--- lab/main.c
+++ lab/main.c
@@ -4,11 +4,20 @@
 // now, don't worry about it
 static int (*fib_asm)(int) = (int (*)(int))0x8000U;
 
+int fib_c(int n) {
+    if (n == 0 || n == 1) {
+        return n;
+    } else {
+        return n + fib_c(n - 1);
+    }
+}
+
 int main(void) {
     int i;
 
     for (i = 0; i <= 32; i++) {
         printf("fib_asm(%d) = %d\n", i, fib_asm(i));
+        printf("fib_c(%d) = %d\n", i, fib_c(i));
     }
 
     return 0;
